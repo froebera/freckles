b5104052bea198bb778b7c3af51d060e2969e2da
Alexander Froeber <Alexander.froeber@scoop-software.de>
2023-10-04 11:24:21 +0200

    Updated kitty config. aliases for git in fish

---
+++ fish/conf.d/git.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/fish
+
+alias g="git"
+alias ga="git add"
+alias gaa="git add --all"
+alias gsb="git status --short -b"
+
+alias gco="git checkout"
+alias gcb="git checkout -b"
+
+alias glol="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+
+alias gp="git push"
+
+alias gri="git rebase --i"
---
+++ setup.md
@@ -0,0 +1,10 @@
+sudo apt install -y fish curl exa zip unzip
+
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl -s "https://get.sdkman.io" | bash
+
+https://gitlab.com/dwt1/shell-color-scripts
+
